5f9336a05de045ece6f1ca1ba145fe0ac7ec417d
Hamayoun <hamayounemail@gmail.com>
2022-08-20 16:53:42 +0430

    updated the project

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,31 +1,32 @@
-import React, { Component } from "react"
+/* eslint-disable */
+import React, { Component } from 'react';
 
 class InputTodo extends Component {
-    state = {
-        title: ""
-      };
+  state = {
+    title: '',
+  };
 
-      onChange = e => {
-        this.setState({
-          title: e.target.value
-        });
-      };
+  onChange = (e) => {
+    this.setState({
+      title: e.target.value,
+    });
+  };
 
-      handleSubmit = e => {
-        e.preventDefault()
-        if (this.state.title.trim()) {
-          this.props.addTodoProps(this.state.title)
-          this.setState({
-            title: "",
-          })
-        } else {
-          alert("Please write item")
-        }
-      }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.title.trim()) {
+      this.props.addTodoProps(this.state.title);
+      this.setState({
+        title: '',
+      });
+    } else {
+      alert('Please write item');
+    }
+  };
 
   render() {
     return (
-        <form onSubmit={this.handleSubmit} className="form-container">
+      <form onSubmit={this.handleSubmit} className="form-container">
         <input
           type="text"
           className="input-text"
@@ -36,7 +37,7 @@ class InputTodo extends Component {
         />
         <button className="input-submit">Submit</button>
       </form>
-    )
+    );
   }
 }
-export default InputTodo
+export default InputTodo;
